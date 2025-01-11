1b3b023be0bc843e280ef071c57fc4271a7325ac
Christina Ju <christinajj314@gmail.com>
2025-01-11 11:25:16 -0800

    'usermodel & recipemodel'

---
+++ my-react-app/server/models/recipeModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  ingredients: { type: String, required: false },
+  directions: { type: String, required: false },
+  category: { type: String, required: false },
+});
+
+modules.exports = mongoose.model('Recipe', recipeSchema);
---
+++ my-react-app/server/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /\S+@\S+\.\S+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email.`,
+    },
+  },
+  password: { type: String, required: true },
+  savedRecipe: [{ type: mongoose.Schema.types.ObjectId, ref: 'Recipe' }],
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+modules.exports = mongoose.model('User', userSchema);
