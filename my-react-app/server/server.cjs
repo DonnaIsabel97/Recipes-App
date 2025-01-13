b349633485f0cf5eb33c690afffc06ca3e208a42
DonnaIsabel97 <tpr.th.bvb@gmail.com>
2025-01-13 17:09:19 -0500

    server

--- my-react-app/server/server.cjs
+++ my-react-app/server/server.cjs
@@ -1,23 +1,23 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 8080;
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const userController = require('./controllers/userController');
-const recipesController = require('./controllers/recipeController');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const userController = require("./controllers/userController");
+const recipesController = require("./controllers/recipeController");
 
 dotenv.config();
-console.log('MongoDB URI:', process.env.MONGODB_URI);
-console.log('Session secret:', process.env.SESSION_SECRET);
+console.log("MongoDB URI:", process.env.MONGODB_URI);
+console.log("Session secret:", process.env.SESSION_SECRET);
 app.use(
   cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'DELETE', 'PUT'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
@@ -25,12 +25,12 @@ app.use(express.json());
 
 // Serve static files from the dist folder
 // app.use(express.static(path.resolve(__dirname, "../dist"))),
-console.log('Connecting to MongoDB with URI:', process.env.MONGODB_URI); // this its undefined - fixed :)
+console.log("Connecting to MongoDB with URI:", process.env.MONGODB_URI); // this its undefined - fixed :)
 
 app.use(
   session({
     // secret key for session encryption
-    secret: process.env.SESSION_SECRET || 'your-secret-key',
+    secret: process.env.SESSION_SECRET || "your-secret-key",
     // don't fore resave of session
     resave: false,
     // don't save empty session
@@ -41,36 +41,39 @@ app.use(
       // mongodb connection uri
       mongoUrl: process.env.MONGODB_URI,
       // session collection name in mongodb
-      collectionName: 'sessions',
+      collectionName: "sessions",
     }),
   })
 );
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
+  .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
 // GET request
 
 // Root
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+app.get("/", (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, "../index.html"));
 });
 
-app.get('/recipes', recipesController.getRecipes, (req, res) => {
+app.get("/recipes", recipesController.getRecipes, (req, res) => {
   return res.status(200).send(res.locals.recipes);
 });
 
 // POST request
 
 // Sign up route with response
-app.post('/createUser', userController.createUser, (req, res) => {
+app.post("/createUser", userController.createUser, (req, res) => {
+  const { email } = res.locals.newUser;
+
+  console.log(`${email} signed up successfully`);
   return res.status(200).send(res.locals.newUser);
 });
 
 // Log in route with response
-app.post('/verifyUser', userController.verifyUser, (req, res) => {
+app.post("/verifyUser", userController.verifyUser, (req, res) => {
   return res.status(200).send(res.locals.user);
 });
 
@@ -79,7 +82,7 @@ app.use((err, req, res, next) => {
   console.error(err.log || err.message);
   res
     .status(err.status || 500)
-    .send({ error: err.message || 'An unexpected error occurred' });
+    .send({ error: err.message || "An unexpected error occurred" });
 });
 
 app.listen(PORT, () => {
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,25 +1,25 @@
-import Dashboard from './Dashboard';
-import RecipeDetails from './RecipeDetails';
-import Navbar from './Navbar';
-import Profile from './Profile';
-import Login from './Login';
-import Signup from './Signup';
-import { Route, Routes, useLocation } from 'react-router-dom';
-
+import Dashboard from "./Dashboard";
+import RecipeDetails from "./RecipeDetails";
+import Navbar from "./Navbar";
+import Profile from "./Profile";
+import Login from "./Login";
+import Signup from "./Signup";
+import { Route, Routes, useLocation } from "react-router-dom";
 function App() {
   const location = useLocation();
   return (
     <div>
-      {location.pathname !== '/signup' && location.pathname !== '/' && <Navbar />}
+      {location.pathname !== "/signup" && location.pathname !== "/" && (
+        <Navbar />
+      )}
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/search' element={<Dashboard />} />
-        <Route path='/recipe/:idMeal' element={<RecipeDetails />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/search" element={<Dashboard />} />
+        <Route path="/recipe/:idMeal" element={<RecipeDetails />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
 }
-
 export default App;
